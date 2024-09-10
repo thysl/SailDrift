3bbacffcde8f04ddb6833d7cdd18c850a3b2e9b8
thysl <vlqxgaming@gmail.com>
2024-09-10 19:15:32 +0200

    Fixed screen resolution + small step to scrolling background

--- code/background_scroll.py
+++ code/background_scroll.py
@@ -9,9 +9,15 @@ class water_tile:
         self.IMG = pygame.image.load('./assets/images/water_tile.png')
         self.IMG = pygame.transform.scale(self.IMG, (WIN_WIDTH, WIN_HEIGHT))
 
+        self.WIDTH = self.IMG.get_width()
+        self.HEIGHT = self.IMG.get_height()
+
         self.x = x
         self.y = y
 
 water_tiles = []
 
-water_tiles.append(water_tile(0, 0))
+water_tiles.append(water_tile(-(water_tile(0, 0).WIDTH / 2), -(water_tile(0, 0).HEIGHT / 2)))
+water_tiles.append(water_tile((water_tile(0, 0).WIDTH / 2), (water_tile(0, 0).HEIGHT / 2)))
+water_tiles.append(water_tile(-(water_tile(0, 0).WIDTH / 2), (water_tile(0, 0).HEIGHT / 2)))
+water_tiles.append(water_tile((water_tile(0, 0).WIDTH / 2), -(water_tile(0, 0).HEIGHT / 2)))
--- code/global_vars.py
+++ code/global_vars.py
@@ -1,6 +1,9 @@
 import pygame
 
-WIN_WIDTH, WIN_HEIGHT = 1920, 1080
-WIN = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
-WIN_WIDTH, WIN_HEIGHT = pygame.display.get_window_size()[0], pygame.display.get_window_size()[1]
+pygame.display.init()
+
+WIN_WIDTH, WIN_HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_w/16*9
+WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption("SailDrift Simulator")
+
 FPS = 60
